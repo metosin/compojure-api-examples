80e89a718b2281fd021ad82ee852c1889ab46ffe
Tommi Reiman <tommi.reiman@metosin.fi>
2014-01-25 22:51:59 +0200

    remove nicks

--- src/compojure/api/examples/handler.clj
+++ src/compojure/api/examples/handler.clj
@@ -13,18 +13,15 @@
       (GET* "/pizzas" []
         :return   Pizza
         :summary  "Gets all Pizzas"
-        :nickname "getPizzas"
         (ok (get-pizzas)))
       (GET* "/pizzas/:id" [id]
         :return   Pizza
         :summary  "Gets a pizza"
-        :nickname "getPizza"
         (ok (get-pizza (->Long id))))
       (POST* "/pizzas" []
         :return   Pizza
         :body     [pizza NewPizza {:description "new pizza"}]
         :summary  "Adds a pizza"
-        :nickname "addPizza"
         (ok (add! pizza)))
       (PUT* "/pizzas" []
         :return   Pizza
@@ -34,5 +31,4 @@
       (DELETE* "/pizzas/:id" [id]
         :return   Pizza
         :summary  "Deletes a Pizza"
-        :nickname "deletePizza"
         (ok (delete! (->Long id)))))))
