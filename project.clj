651a61496e49b1bf3590eb375212d840818131e7
Tommi Reiman <tommi.reiman@metosin.fi>
2014-01-25 23:37:18 +0200

    use embedded jetty9

--- project.clj
+++ project.clj
@@ -1,12 +1,13 @@
 (defproject metosin/compojure-api-examples "0.0.1"
   :description "Compojure-api-examples"
   :dependencies [[org.clojure/clojure "1.5.1"]
+                 [info.sunng/ring-jetty9-adapter "0.3.0"]
                  [prismatic/schema "0.2.0"]
                  [metosin/compojure-api "0.0.1"]
                  [metosin/ring.swagger-ui "0.0.1"]]
-  :plugins [[lein-ring "0.8.7"]]
-  :ring {:handler compojure.api.examples.handler/app}
-  :profiles {:uberjar {:resource-paths ["swagger-ui"]}
+  :main compojure.api.examples.server
+  :profiles {:uberjar {:aot :all
+                       :resource-paths ["swagger-ui"]}
              :dev {:plugins [[lein-clojars "0.9.1"]
                              [lein-midje "3.1.1"]]
                    :dependencies [[ring-mock "0.1.5"]
---
+++ src/compojure/api/examples/server.clj
@@ -0,0 +1,7 @@
+(ns compojure.api.examples.server
+  (:require [ring.adapter.jetty9 :refer [run-jetty]]
+            [compojure.api.examples.handler :refer [app]])
+  (:gen-class))
+
+(defn -main [& _] (run-jetty app {:port 3000}))
+
