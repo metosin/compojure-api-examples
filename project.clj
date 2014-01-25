2c587e5c00765ac73cdb58feb2f9a3fa12e54eca
Tommi Reiman <tommi.reiman@metosin.fi>
2014-01-25 22:52:59 +0200

    golfing project.clj

--- project.clj
+++ project.clj
@@ -1,18 +1,12 @@
 (defproject metosin/compojure-api-examples "0.0.1"
   :description "Compojure-api-examples"
-  :url "https://github.com/metosin/compojure-api-examples"
-  :license {:name "Eclipse Public License - v 1.0"
-            :url "http://www.eclipse.org/legal/epl-v10.html"
-            :distribution :repo
-            :comments "same as Clojure"}
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [prismatic/schema "0.2.0"]
                  [metosin/compojure-api "0.0.1"]
                  [metosin/ring.swagger-ui "0.0.1"]]
   :plugins [[lein-ring "0.8.7"]]
   :ring {:handler compojure.api.examples.handler/app}
-  :profiles {:uberjar {;;:aot :all
-                       :resource-paths ["swagger-ui"]}
+  :profiles {:uberjar {:resource-paths ["swagger-ui"]}
              :dev {:plugins [[lein-clojars "0.9.1"]
                              [lein-midje "3.1.1"]]
                    :dependencies [[ring-mock "0.1.5"]
