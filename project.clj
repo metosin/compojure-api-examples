05933ad24dcf8bf740eba9f18ecf7557c65a9cd0
Tommi Reiman <tommi.reiman@metosin.fi>
2014-01-26 15:52:49 +0200

    use compojure.api.main as the main

--- project.clj
+++ project.clj
@@ -3,7 +3,8 @@
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [info.sunng/ring-jetty9-adapter "0.3.0"]
                  [metosin/compojure-api "0.0.1"]]
-  :main compojure.api.examples.main
-  :profiles {:uberjar {:aot [compojure.api.examples.main]
+  :main compojure.api.main
+  :start compojure.api.examples.server/start
+  :profiles {:uberjar {:aot [compojure.api.main]
                        :resource-paths ["swagger-ui"]}
              :dev {:dependencies [[javax.servlet/servlet-api "2.5"]]}})
--- src/compojure/api/examples/main.clj
+++
@@ -1,6 +0,0 @@
-(ns compojure.api.examples.main
-  (:gen-class))
-
-(defn -main [& args]
-  (require 'compojure.api.examples.server)
-  (apply (resolve 'compojure.api.examples.server/start) args))
