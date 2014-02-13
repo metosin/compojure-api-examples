486e245cb724b0e3615d65ae2ac1675a2c1c481f
Tommi Reiman <tommi.reiman@metosin.fi>
2014-02-13 22:46:11 +0200

    bump up versions

--- project.clj
+++ project.clj
@@ -1,12 +1,12 @@
-(defproject metosin/compojure-api-examples "0.1.0"
+(defproject metosin/compojure-api-examples "0.2.0"
   :description "Compojure-api-examples"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"
             :distribution :repo
             :comments "same as Clojure"}
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [metosin/compojure-api "0.3.0"]
-                 [metosin/ring-http-response "0.3.0"]
+                 [metosin/compojure-api "0.4.0"]
+                 [metosin/ring-http-response "0.4.0"]
                  [metosin/ring-swagger-ui "2.0.7"]]
   :ring {:handler compojure.api.examples.handler/app}
   :profiles {:uberjar {:resource-paths ["swagger-ui"]}
