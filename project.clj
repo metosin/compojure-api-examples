3021dad86e5cc534dc50380aec36166e35f5829d
Tommi Reiman <tommi@iki.fi>
2014-03-25 10:56:33 +0200

    bump up versions

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
-(defproject metosin/compojure-api-examples "0.8.0"
+(defproject metosin/compojure-api-examples "0.8.4"
   :description "Compojure-api-examples"
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [metosin/compojure-api "0.8.2"]
-                 [metosin/ring-http-response "0.3.0"]
+                 [metosin/compojure-api "0.8.4"]
+                 [metosin/ring-http-response "0.4.0"]
                  [metosin/ring-swagger-ui "2.0.12-1"]]
   :ring {:handler compojure.api.examples.handler/app}
   :uberjar-name "examples.jar"
