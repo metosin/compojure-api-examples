af842dcc9f34f48931215ee119c68a83f2747ead
Tommi Reiman <tommi@iki.fi>
2014-05-07 22:51:31 +0300

    0.11.2, remove implicit clj-time dependency as the new Ring uses now newer version.

--- project.clj
+++ project.clj
@@ -1,8 +1,7 @@
-(defproject metosin/compojure-api-examples "0.11.0"
+(defproject metosin/compojure-api-examples "0.11.2"
   :description "Compojure-api-examples"
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [clj-time "0.7.0"]
-                 [metosin/compojure-api "0.11.0"]
+                 [metosin/compojure-api "0.11.2"]
                  [metosin/ring-http-response "0.4.0"]
                  [metosin/ring-swagger-ui "2.0.12-1"]]
   :ring {:handler compojure.api.examples.handler/app}
