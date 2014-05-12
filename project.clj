e40366d6abf50b410aa35243fe18d0dde2df1af5
Juho Teperi <juho.teperi@iki.fi>
2014-05-12 11:10:48 +0300

    Update deps

--- project.clj
+++ project.clj
@@ -1,9 +1,10 @@
-(defproject metosin/compojure-api-examples "0.11.2"
+(defproject metosin/compojure-api-examples "0.11.3"
   :description "Compojure-api-examples"
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [metosin/compojure-api "0.11.2"]
+                 [metosin/compojure-api "0.11.3"]
                  [metosin/ring-http-response "0.4.0"]
-                 [metosin/ring-swagger-ui "2.0.16-2"]]
+                 [metosin/ring-swagger-ui "2.0.16-2"]
+                 [clj-time "0.7.0"]]
   :ring {:handler compojure.api.examples.handler/app}
   :uberjar-name "examples.jar"
   :profiles {:uberjar {:resource-paths ["swagger-ui"]}
