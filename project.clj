faeb11c79c2908dd913600e9f67cb4aefc25b0f3
Tommi Reiman <tommi@iki.fi>
2014-06-28 17:05:16 +0200

    bump up version

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
-(defproject metosin/compojure-api-examples "0.13.2"
+(defproject metosin/compojure-api-examples "0.13.3"
   :description "Compojure-api-examples"
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [metosin/compojure-api "0.13.2"]
+                 [metosin/compojure-api "0.13.3"]
                  [metosin/ring-http-response "0.4.0"]
                  [metosin/ring-swagger-ui "2.0.16-3"]
                  [clj-time "0.7.0"]]
