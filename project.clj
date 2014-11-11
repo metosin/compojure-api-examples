6b7e789a4551549fc854eb30ba8a82455e8d3a4f
Tommi Reiman <tommi@iki.fi>
2014-11-11 21:57:03 +0200

    bump up version

--- project.clj
+++ project.clj
@@ -1,9 +1,9 @@
-(defproject metosin/compojure-api-examples "0.16.0"
+(defproject metosin/compojure-api-examples "0.16.4"
   :description "Compojure-api-examples"
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [metosin/ring-swagger "0.13.0"]
-                 [metosin/compojure-api "0.16.0"]
-                 [metosin/ring-http-response "0.5.0"]
+                 [metosin/ring-swagger "0.14.1"]
+                 [metosin/compojure-api "0.16.4"]
+                 [metosin/ring-http-response "0.5.2"]
                  [metosin/ring-swagger-ui "2.0.17"]
                  [clj-time "0.8.0"]]
   :ring {:handler compojure.api.examples.handler/app}
@@ -11,4 +11,4 @@
   :profiles {:uberjar {:resource-paths ["swagger-ui"]
                        :aot :all}
              :dev {:dependencies [[javax.servlet/servlet-api "2.5"]]
-                   :plugins [[lein-ring "0.8.11"]]}})
+                   :plugins [[lein-ring "0.8.13"]]}})
