e8e8fffe87d7cd48baec8f84a411ef4e013885b3
Tommi Reiman <tommi@iki.fi>
2015-03-02 21:48:48 +0200

    update deps

--- project.clj
+++ project.clj
@@ -1,14 +1,14 @@
-(defproject metosin/compojure-api-examples "0.16.0"
+(defproject metosin/compojure-api-examples "0.17.0"
   :description "Compojure-api-examples"
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [metosin/ring-swagger "0.13.0"]
-                 [metosin/compojure-api "0.16.0"]
-                 [metosin/ring-http-response "0.5.0"]
-                 [metosin/ring-swagger-ui "2.0.17"]
-                 [clj-time "0.8.0"]]
+                 [metosin/ring-swagger "0.18.1"]
+                 [metosin/compojure-api "0.18.0-SNAPSHOT"]
+                 [metosin/ring-http-response "0.6.0"]
+                 [metosin/ring-swagger-ui "2.0.24"]
+                 [clj-time "0.9.0"]]
   :ring {:handler compojure.api.examples.handler/app}
   :uberjar-name "examples.jar"
   :profiles {:uberjar {:resource-paths ["swagger-ui"]
                        :aot :all}
              :dev {:dependencies [[javax.servlet/servlet-api "2.5"]]
-                   :plugins [[lein-ring "0.8.11"]]}})
+                   :plugins [[lein-ring "0.9.2"]]}})
