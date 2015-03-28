1c56d0d56675ef05e7dcef94d9a817d7435234a5
Tommi Reiman <tommi@iki.fi>
2015-03-28 11:00:54 +0200

    [metosin/ring-swagger "0.19.1"] is available but we use "0.18.1" [metosin/compojure-api "0.18.0"] is available but we use "0.18.0-SNAPSHOT" [metosin/ring-http-response "0.6.1"] is available but we use "0.6.0" [lein-ring "0.9.3"] is available but we use "0.9.2"

--- project.clj
+++ project.clj
@@ -1,9 +1,9 @@
 (defproject metosin/compojure-api-examples "0.17.0"
   :description "Compojure-api-examples"
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [metosin/ring-swagger "0.18.1"]
-                 [metosin/compojure-api "0.18.0-SNAPSHOT"]
-                 [metosin/ring-http-response "0.6.0"]
+                 [metosin/ring-swagger "0.19.1"]
+                 [metosin/compojure-api "0.18.0"]
+                 [metosin/ring-http-response "0.6.1"]
                  [metosin/ring-swagger-ui "2.1.8-M1"]
                  [clj-time "0.9.0"]]
   :ring {:handler compojure.api.examples.handler/app}
@@ -11,4 +11,4 @@
   :profiles {:uberjar {:resource-paths ["swagger-ui"]
                        :aot :all}
              :dev {:dependencies [[javax.servlet/servlet-api "2.5"]]
-                   :plugins [[lein-ring "0.9.2"]]}})
+                   :plugins [[lein-ring "0.9.3"]]}})
