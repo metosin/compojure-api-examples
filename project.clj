9890437c5db772a451b56206a2161b0a9b1132ce
Tommi Reiman <tommi@iki.fi>
2015-05-01 21:58:35 +0300

    update dependencies

--- project.clj
+++ project.clj
@@ -1,11 +1,9 @@
 (defproject metosin/compojure-api-examples "0.20.0"
   :description "Compojure-api-examples"
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [metosin/ring-swagger "0.20.0"]
                  [metosin/compojure-api "0.20.0"]
                  [metosin/ring-http-response "0.6.1"]
-                 [metosin/ring-swagger-ui "2.1.0-M2-2"]
-                 [clj-time "0.9.0"]]
+                 [metosin/ring-swagger-ui "2.1.1-M2"]]
   :ring {:handler compojure.api.examples.handler/app}
   :uberjar-name "examples.jar"
   :profiles {:uberjar {:resource-paths ["swagger-ui"]
