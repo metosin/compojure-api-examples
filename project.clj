b41fb5847d8ef61eaaa6282f228cbfacd3a1ad22
Tommi Reiman <tommi@iki.fi>
2015-05-17 13:32:15 +0300

    bump up versions

--- project.clj
+++ project.clj
@@ -1,12 +1,14 @@
-(defproject metosin/compojure-api-examples "0.20.0"
+(defproject metosin/compojure-api-examples "0.20.3"
   :description "Compojure-api-examples"
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [metosin/compojure-api "0.20.0"]
+                 [clj-time "0.9.0"] ;; needed as `lein ring` is broken.
+                 [metosin/compojure-api "0.20.3"]
                  [metosin/ring-http-response "0.6.1"]
                  [metosin/ring-swagger-ui "2.1.1-M2"]]
   :ring {:handler compojure.api.examples.handler/app}
   :uberjar-name "examples.jar"
+  :uberwar-name "examples.war"
   :profiles {:uberjar {:resource-paths ["swagger-ui"]
                        :aot :all}
              :dev {:dependencies [[javax.servlet/servlet-api "2.5"]]
-                   :plugins [[lein-ring "0.9.3"]]}})
+                   :plugins [[lein-ring "0.9.4"]]}})
