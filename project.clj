2f1adfd86a68c01cae7db113d957988cf47c61ac
Tommi Reiman <tommi@iki.fi>
2016-10-24 16:48:43 +0200

    Fixed project

--- project.clj
+++ project.clj
@@ -1,12 +1,11 @@
-(defproject metosin/compojure-api-examples "1.0.0"
+(defproject metosin/compojure-api-examples "1.0.1"
   :description "Compojure-api-examples"
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [clj-time "0.11.0"] ;; needed as `lein ring uberwar` is broken.
-                 [metosin/compojure-api "1.0.0"]]
+                 [metosin/compojure-api "1.1.9"]]
   :ring {:handler compojure.api.examples.handler/app}
   :uberjar-name "examples.jar"
   :uberwar-name "examples.war"
   :profiles {:uberjar {:resource-paths ["swagger-ui"]
                        :aot :all}
              :dev {:dependencies [[javax.servlet/servlet-api "2.5"]]
-                   :plugins [[lein-ring "0.9.7"]]}})
+                   :plugins [[ikitommi/lein-ring "0.9.8-FIX"]]}})
--- src/compojure/api/examples/handler.clj
+++ src/compojure/api/examples/handler.clj
@@ -4,96 +4,99 @@
             [compojure.api.examples.domain :refer :all]
             [schema.core :as s]))
 
-(defapi app
-  {:swagger
-   {:info {:title "Sample Api"
-           :description "Compojure Api sample application"}
-    :tags [{:name "math", :description "math with parameters"}
-           {:name "echo", :description "request echoes"}
-           {:name "pizza", :description "pizza Api it is."}]}}
+(def app
+  (api
+    {:swagger
+     {:ui "/"
+      :spec "/swagger.json"
+      :data {:info {:title "Sample Api"
+                    :description "Compojure Api sample application"}
+             :tags [{:name "math", :description "math with parameters"}
+                    {:name "echo", :description "request echoes"}
+                    {:name "pizza", :description "pizza Api it is."}]}}}
 
-  (context "/math" []
-    :tags ["math"]
+    (context "/math" []
+      :tags ["math"]
 
-    (GET "/plus" []
-      :return Total
-      :query-params [x :- Long, y :- Long]
-      :summary "x+y with query-parameters"
-      (ok {:total (+ x y)}))
+      (GET "/plus" []
+        :return Total
+        :query-params [x :- Long, y :- Long]
+        :summary "x+y with query-parameters"
+        (ok {:total (+ x y)}))
 
-    (POST "/minus" []
-      :return Total
-      :body-params [x :- Long, y :- Long]
-      :summary "x-y with body-parameters"
-      (ok {:total (- x y)}))
+      (POST "/minus" []
+        :return Total
+        :body-params [x :- Long, y :- Long]
+        :summary "x-y with body-parameters"
+        (ok {:total (- x y)}))
 
-    (GET "/times/:x/:y" []
-      :return Total
-      :path-params [x :- Long, y :- Long]
-      :summary "x*y with path-parameters"
-      (ok {:total (* x y)}))
+      (GET "/times/:x/:y" []
+        :return Total
+        :path-params [x :- Long, y :- Long]
+        :summary "x*y with path-parameters"
+        (ok {:total (* x y)}))
 
-    (GET "/power" []
-      :return Total
-      :header-params [x :- Long, y :- Long]
-      :summary "x^y with header-parameters"
-      (ok {:total (long (Math/pow x y))})))
+      (GET "/power" []
+        :return Total
+        :header-params [x :- Long, y :- Long]
+        :summary "x^y with header-parameters"
+        (ok {:total (long (Math/pow x y))})))
 
-  (context "/echo" []
-    :tags ["echo"]
+    (context "/echo" []
+      :tags ["echo"]
 
-    (GET "/request" req
-      (ok (dissoc req :body)))
+      (GET "/request" req
+        (ok (dissoc req :body)))
 
-    (GET "/pizza" []
-      :return NewSingleToppingPizza
-      :query [pizza NewSingleToppingPizza]
-      :summary "get echo of a pizza"
-      (ok pizza))
+      (GET "/pizza" []
+        :return NewSingleToppingPizza
+        :query [pizza NewSingleToppingPizza]
+        :summary "get echo of a pizza"
+        (ok pizza))
 
-    (PUT "/anonymous" []
-      :return [{:secret Boolean s/Keyword s/Any}]
-      :body [body [{:secret Boolean s/Keyword s/Any}]]
-      (ok body))
+      (PUT "/anonymous" []
+        :return [{:secret Boolean s/Keyword s/Any}]
+        :body [body [{:secret Boolean s/Keyword s/Any}]]
+        (ok body))
 
-    (GET "/hello" []
-      :return String
-      :query-params [name :- String]
-      (ok (str "Hello, " name)))
+      (GET "/hello" []
+        :return String
+        :query-params [name :- String]
+        (ok (str "Hello, " name)))
 
-    (POST "/pizza" []
-      :return NewSingleToppingPizza
-      :body [pizza NewSingleToppingPizza]
-      :summary "post echo of a pizza"
-      (ok pizza)))
+      (POST "/pizza" []
+        :return NewSingleToppingPizza
+        :body [pizza NewSingleToppingPizza]
+        :summary "post echo of a pizza"
+        (ok pizza)))
 
-  (context "/pizzas" []
-    :tags ["pizza"]
+    (context "/pizzas" []
+      :tags ["pizza"]
 
-    (GET "/" []
-      :return [Pizza]
-      :summary "Gets all Pizzas"
-      (ok (get-pizzas)))
+      (GET "/" []
+        :return [Pizza]
+        :summary "Gets all Pizzas"
+        (ok (get-pizzas)))
 
-    (POST "/" []
-      :return Pizza
-      :body [pizza NewPizza {:description "new pizza"}]
-      :summary "Adds a pizza"
-      (ok (add! pizza)))
+      (POST "/" []
+        :return Pizza
+        :body [pizza NewPizza {:description "new pizza"}]
+        :summary "Adds a pizza"
+        (ok (add! pizza)))
 
-    (PUT "/" []
-      :return Pizza
-      :body [pizza Pizza]
-      :summary "Updates a pizza"
-      (ok (update! pizza)))
+      (PUT "/" []
+        :return Pizza
+        :body [pizza Pizza]
+        :summary "Updates a pizza"
+        (ok (update! pizza)))
 
-    (GET "/:id" []
-      :return Pizza
-      :path-params [id :- Long]
-      :summary "Gets a pizza"
-      (ok (get-pizza id)))
+      (GET "/:id" []
+        :return Pizza
+        :path-params [id :- Long]
+        :summary "Gets a pizza"
+        (ok (get-pizza id)))
 
-    (DELETE "/:id" []
-      :path-params [id :- Long]
-      :summary "Deletes a Pizza"
-      (ok (delete! id)))))
+      (DELETE "/:id" []
+        :path-params [id :- Long]
+        :summary "Deletes a Pizza"
+        (ok (delete! id))))))
